9385889feaba35f77c182d65dbeef57a91b30e91
CNRui <13266776177@163.com>
2017-12-21 17:35:10 +0800

    第5次提交

---
+++ SparkExamples/src/SQL/RDDRelation.scala
@@ -0,0 +1,43 @@
+package SQL
+
+import org.apache.spark.sql.SaveMode
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.{SparkConf, SparkContext}
+
+object RDDRelation {
+
+  case class Record(key: Int, value: String)
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("RDDRelation").setMaster("local")
+    val sc = new SparkContext(conf)
+    val hc = new HiveContext(sc)
+
+    import hc.implicits._
+
+    val df = hc.createDataFrame((0 to 100).map(i => Record(i, s"val_$i")))
+
+    df.registerTempTable("records")
+
+    println("Result of SELECT:")
+    hc.sql("select * from records limit 10").show()
+
+    println("Result of COUNT:")
+    hc.sql("select count(*) from records").show()
+
+    val rddFromSql = hc.sql("select * from records where key<15")
+    println("Result of RDD:")
+    rddFromSql.rdd.map(row => s"key:${row(0)},value:${row(1)}").collect.foreach(println)
+
+    df.write.mode(SaveMode.Overwrite).parquet("hdfs://mycluster/test/pair.parquet")
+
+    val parquetFile = hc.read.parquet("hdfs://mycluster/test/pair.parquet")
+
+    parquetFile.where($"key" === 1).select($"value".as("a")).collect.foreach(println)
+
+    sc.stop()
+
+
+  }
+
+}
---
+++ SparkExamples/src/SQL/SparkHiveExample.scala
@@ -0,0 +1,51 @@
+package SQL
+
+import java.io.File
+
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.{SparkConf, SparkContext}
+
+object SparkHiveExample {
+
+  case class Record(key: Int, value: String)
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("SparkHiveExample").setMaster("local")
+    val sc = new SparkContext(conf)
+    val hc = new HiveContext(sc)
+
+    import hc.implicits._
+    import hc.sql
+
+    //   sql("CREATE TABLE IF NOT EXISTS test.src (key INT, value STRING)")
+    //   sql("LOAD DATA LOCAL INPATH 'E:/github/SparkExamples/resources/kv1.txt' INTO TABLE test.src")
+    sql("show databases").foreach(println)
+    sql("SELECT * FROM test.src").show()
+
+    sql("SELECT COUNT(*) FROM test.src").show()
+
+
+    val sqlDF = sql("SELECT key, value FROM test.src WHERE key < 10 ORDER BY key")
+
+
+
+    val stringsDS = sqlDF.map {
+      case Row(key: Int, value: String) => s"Key: $key, Value: $value"
+    }
+
+
+    stringsDS.toDF().show()
+
+    val recordsDF = hc.createDataFrame((1 to 100).map(i => Record(i, s"var_$i")))
+
+    recordsDF.registerTempTable("records")
+
+    sql("SELECT r.* FROM records r JOIN test.src s ON r.key = s.key").show()
+
+
+    sc.stop()
+
+  }
+
+}
