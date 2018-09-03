cc0b36f330300b8afb6ef8b8a2980414c57a17ad
zhanrui <zhanrui@globalegrow.com>
2018-09-03 11:30:08 +0800

    1.更新GroupByTest

--- SparkExamples/src/SparkExamples/GroupByTest.scala
+++ SparkExamples/src/SparkExamples/GroupByTest.scala
@@ -1,4 +1,5 @@
 package SparkExamples
+
 import java.util.Random
 
 import org.apache.spark.{SparkConf, SparkContext}
@@ -9,28 +10,30 @@ import org.apache.spark.{SparkConf, SparkContext}
 object GroupByTest {
 
   def main(args: Array[String]): Unit = {
-    val numMappers=if (args.length>0) args(0).toInt else 2
-    val numKVPairs=if (args.length>1) args(1).toInt else 1000
-    val keySize=if (args.length>2) args(2).toInt else 1000
-    val numReducers=if (args.length>3) args(3).toInt else numMappers
-    val ranMax=numMappers*numKVPairs
+    val numMappers = if (args.length > 0) args(0).toInt else 2
+    val numKVPairs = if (args.length > 1) args(1).toInt else 1000
+    val keySize = if (args.length > 2) args(2).toInt else 1000
+    val numReducers = if (args.length > 3) args(3).toInt else numMappers
+    val ranMax = numMappers * numKVPairs
 
-    val conf=new SparkConf().setAppName("GroupByTest").setMaster("local")
-    val sc=new SparkContext(conf)
+    val conf = new SparkConf().setAppName("GroupByTest").setMaster("local")
+    val sc = new SparkContext(conf)
 
-    val pairs=sc.parallelize(0 until numMappers,numMappers).flatMap(p=>{
-      val arr=new Array[(Int,Array[Byte])](numKVPairs)
-      for (i<-0 until numKVPairs){
-        val ranGen=new Random
-        val byteArr=new Array[Byte](keySize)
+    val pairs = sc.parallelize(0 until numMappers, numMappers).flatMap(p => {
+      val arr = new Array[(Int, Array[Byte])](numKVPairs)
+      for (i <- 0 until numKVPairs) {
+        val ranGen = new Random
+        val byteArr = new Array[Byte](keySize)
         ranGen.nextBytes(byteArr)
-        arr(i)=(ranGen.nextInt(ranMax),byteArr)
+        arr(i) = (ranGen.nextInt(ranMax), byteArr)
       }
       arr
     }).cache()
 
-    pairs.count()
+    val a = pairs.count()
+    println(a)
     println(pairs.groupByKey(numReducers).count)
+    println(pairs.toDebugString)
 
   }
 
